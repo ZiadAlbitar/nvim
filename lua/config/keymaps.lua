9addd6a2345ee9c264656998b3e7ce71c95288d8
Ziad Albitar <ziad@Ziads-MacBook-Air.local>
2026-06-03 19:26:09 +0200

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+vim.keymap.set('n', '<leader>h', '<C-w>h', opts)
+vim.keymap.set('n', '<leader>l', '<C-w>l', opts)
+vim.keymap.set('n', '<leader>j', '<C-w>j', opts)
+vim.keymap.set('n', '<leader>k', '<C-w>k', opts)
+vim.keymap.set('t', '<esc>', '<C-\\><C-n>', opts)
+vim.keymap.set('n', '<leader>t', ':belowright :term<CR>:resize 15<CR>', opts)
